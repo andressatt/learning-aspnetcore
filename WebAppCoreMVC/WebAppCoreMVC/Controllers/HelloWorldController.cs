f76d778760c981de8c6b8d200058163144eb4ce7
atsuruda <atsuruda>
2022-11-25 17:09:21 -0300

    Aula 7 - Projeto MVC com .NET Core, EF Core, conexão com BD, migration, scaffolding (CRUD de Movies).

---
+++ WebAppCoreMVC/WebAppCoreMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppCoreMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return "Controller HelloWorld - Método: Index";
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes)
+        {
+            //return HtmlEncoder.Default.Encode($"Seja bem-vindo: {name}! Nº vezes: {numTimes}.");
+            
+            ViewData["Msg"] = "Olá " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
