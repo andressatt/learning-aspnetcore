30056877eeb36dadaea8ea71957c9b0d71b4842b
atsuruda <atsuruda>
2022-11-30 17:10:50 -0300

    Aula 9 - Projeto ASP.NET Core Web API com suporte ao OpenAPI.

---
+++ API.Web.Introducao/API.Web.Introducao/Controllers/NomesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Web.Introducao.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NomesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetNomes()
+        {
+            string[] nomes = { "John", "Mary", "Kelly", "Sean" };
+
+            return nomes;
+        }
+    }
+}
---
+++ API.Web.Introducao/API.Web.Introducao/Controllers/ReceitasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Web.Introducao.Controllers
+{
+    [ApiController]
+    [Route("api/receitas")]
+    public class ReceitasController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetReceitas()
+        {
+            string[] bolos = { "Laranja", "Cenoura", "Chocolate", "Fubá" };
+
+            return bolos;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteReceita(string id)
+        {
+            bool receitaRuim = false;
+
+            return receitaRuim ? BadRequest() : NoContent();
+        }
+    }
+}
